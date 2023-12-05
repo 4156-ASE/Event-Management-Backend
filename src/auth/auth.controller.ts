c3927a28e77bb72fc25bf2d742033cd669d81d5a
Xzh0u <xzh0u.sxz@gmail.com>
2023-12-05 14:50:57 -0500

    feat(auth): set public

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthSignInDto, AuthSignUpDto } from './dto/auth.dto';
+import { Public } from './jwt.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('signup')
   async signup(@Body() dto: AuthSignUpDto) {
     return await this.authService.signup(dto);
   }
 
+  @Public()
   @Post('signin')
   async signin(@Body() dto: AuthSignInDto, @Res() res) {
     return await this.authService.signin(dto, res);
